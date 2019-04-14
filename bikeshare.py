f257e4097e1c9f26136924cc8e4cdf114b6d82be
Mariana Pasquini <mpasquini@gmail.com>
2019-04-14 15:36:51 -0300

    Variables created to make the code cleaner

--- bikeshare.py
+++ bikeshare.py
@@ -15,6 +15,14 @@ DAYS = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'satur
 
 FILTER_OPTIONS = ['month', 'day', 'both', 'none']
 
+CITY_QUESTION = '\nWould you like to see data for Chicago, New York or Washington?\n'
+
+FILTER_QUESTION = '\nWould you like to filter the data by month, day, both or not at all? Type "none" for no time filter.\n'
+
+MONTH_QUESTION = '\nWich month? January, February, March, April, May, or June?.\n'
+
+DAY_QUESTION = '\nWich day? Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday.\n'
+
 def get_user_answer(question, filter_options):
     """ Validates user input """
     while True:
@@ -37,20 +45,20 @@ def get_filters():
     print('\nHello! Let\'s explore some US bikeshare data!')
 
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = get_user_answer('\nWould you like to see data for Chicago, New York or Washington?\n', CITIES)
+    city = get_user_answer(CITY_QUESTION, CITIES)
 
     # get user input for month (all, january, february, ... , june)
     # get user input for day of week (all, monday, tuesday, ... sunday)
-    option = get_user_answer('\nWould you like to filter the data by month, day, both or not at all? Type "none" for no time filter.\n', FILTER_OPTIONS)
+    option = get_user_answer(FILTER_QUESTION, FILTER_OPTIONS)
     month = 'all'
     day = 'all'
     if (option == 'month'):
-        month = get_user_answer('\nWich month? January, February, March, April, May, or June?.\n', MONTHS)
+        month = get_user_answer(MONTH_QUESTION, MONTHS)
     elif (option == 'day'):
-        day = get_user_answer('\nWich day? Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday.\n', DAYS)
+        day = get_user_answer(DAY_QUESTION, DAYS)
     elif (option == 'both'):
-        month = get_user_answer('\nWich month? January, February, March, April, May, or June?.\n', MONTHS)
-        day = get_user_answer('\nWich day? Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday.\n', DAYS)
+        month = get_user_answer(MONTH_QUESTION, MONTHS)
+        day = get_user_answer(DAY_QUESTION, DAYS)
 
     print('-'*40)
     return city, month, day
